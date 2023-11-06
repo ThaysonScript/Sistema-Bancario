9ee906d703d97acafed521843dca097efe0dab45
ThaysonScript <thaysonguedes01@gmail.com>
2023-11-06 17:37:21 -0300

    add - models entities

---
+++ src/src/models/Funcionario.java
@@ -0,0 +1,28 @@
+package models;
+
+public class Funcionario extends Pessoa{
+    private String cargo;
+    private double salario;
+
+    public Funcionario(String nome, String cpf, String endereco, String telefone, String cargo, double salario) {
+        super(nome, cpf, endereco, telefone);
+        setCargo(cargo);
+        setSalario(salario);
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
--- src/src/models/Pessoa.java
+++ src/src/models/Pessoa.java
@@ -1,4 +1,47 @@
 package models;
 
 public class Pessoa {
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String telefone;
+
+    public Pessoa(String nome, String cpf, String endereco, String telefone) {
+        setNome(nome);
+        setCpf(cpf);
+        setEndereco(endereco);
+        setTelefone(telefone);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
